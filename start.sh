fba4a02793c3e97ed0a1f27c17815ab29c762748
Tobinator647 <91456646+tobiashain@users.noreply.github.com>
2023-09-17 01:40:48 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,12 @@
 ulimit -Sn `ulimit -Hn` # ROCm is a bitch
 source ./venv/bin/activate
 python3 ./src/main.py "$@"
-deactivate
+# Deactivate the virtual environment using an interactive Python session
+python - <<END
+import os
+import sys
+
+if sys.prefix != sys.base_prefix:
+    os.system('deactivate')
+
+END
